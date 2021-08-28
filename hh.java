ff9af7dcc3984a5571c97f9c5f15198dfa92ee8a
KeshyTaysu <eshileyestudos@gmail.com>
2021-08-28 18:55:26 -0300

    perfect

---
+++ hh.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercicios;
+
+/**
+ *
+ * @author user
+ */
+public class hh {
+     public static void main(String[] args) {
+     double contadeagua;
+     double salario=1000;
+     double conta=1000;
+     double desconto=0.15;
+     double agua=0.02;
+     
+     contadeagua=(conta*agua*desconto);
+     System.out.println(contadeagua);
+     
+    }
+    
+}
+
